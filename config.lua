ef66a57f31376b628a5799ca0cd026e4c5af0cfd
Aditya Nath <84228628+MrEvilGamer@users.noreply.github.com>
2022-07-29 00:06:43 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,8 @@
 Config = {}
 
 Config.CarModel = 'adder' -- Spawn Code of the car 
+Config.Wheel = vector3(990.28, 42.84, 70.50) -- This you have to change
+Config.CarCoords = vector4(975.69, 40.21, 71.71, 360.07) -- Car Coords
 Config.WheelPos = vector3(990.28, 42.84, 70.50) -- Wheel pos
 Config.BaseWheelPos = vector3(983.17, 33.86, 46.46) -- Base wheel Pos
 Config.Amount = 50000 -- Amount
