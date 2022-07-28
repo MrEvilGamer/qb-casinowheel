284b4bfdeb5bbc23e2b1232da04606b3599c478b
Aditya Nath <84228628+MrEvilGamer@users.noreply.github.com>
2022-07-28 15:06:38 +0530

    Update for Gabz Casino
    Configured the coords for Gabz Casino

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
 Config.CarModel = 'adder' -- Spawn Code of the car 
-Config.WheelPos = vector3(949.40, 44.71, 72) -- Wheel pos
-Config.BaseWheelPos = vector3(949.6968, 45.06157, 70.63276) -- Base wheel Pos
+Config.WheelPos = vector3(990.28, 42.84, 70.50) -- Wheel pos
+Config.BaseWheelPos = vector3(983.17, 33.86, 46.46) -- Base wheel Pos
 Config.Amount = 50000 -- Amount
