769a6896ff1ac4032d4add2b1884fed607d7f835
Aditya Nath <84228628+MrEvilGamer@users.noreply.github.com>
2022-07-29 00:44:06 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,6 +3,7 @@ Config = {}
 Config.CarModel = 'adder' -- Spawn Code of the car 
 Config.Wheel = vector3(990.28, 42.84, 70.50) -- This you have to change
 Config.CarCoords = vector4(975.69, 40.21, 71.71, 360.07) -- Car Coords
+Config.WalkPos = vector3(988.16, 42.44, 71.27) -- Where your character walks from
 Config.WheelPos = vector3(990.28, 42.84, 70.50) -- Wheel pos
 Config.BaseWheelPos = vector3(983.17, 33.86, 46.46) -- Base wheel Pos
 Config.Amount = 50000 -- Amount
